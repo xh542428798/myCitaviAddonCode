6dc2b5746297e479457db03ab3dad620e85eb0a8
Hui Xie <xh542428798@hotmail.com>
2025-10-29 23:12:42 +0800

    修复Bug：KnowledgePanelShowPDF

--- KnowledgePanelShowPDF/ClassLibrary1/KnowledgePanelShowPDF.cs
+++ KnowledgePanelShowPDF/ClassLibrary1/KnowledgePanelShowPDF.cs
@@ -1,17 +1,13 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Linq;
+
 using System.Windows.Forms;
 
 using SwissAcademic.Citavi;
 using SwissAcademic.Citavi.Shell;
-using SwissAcademic.Citavi.Shell.Controls.Preview;
-using SwissAcademic.Citavi.Controls.Wpf;
-using SwissAcademic.Citavi.Shell.Controls.SmartRepeaters;
+//using SwissAcademic.Citavi.Shell.Controls.SmartRepeaters;
 using SwissAcademic.Controls;
-using SwissAcademic.Pdf;
 
 using Infragistics.Win.UltraWinToolbars;
 
@@ -30,7 +26,6 @@ namespace KnowledgePanelShowPDF
         {
             MainForm mainForm = (MainForm)hostingForm;
 
-            mainForm.ActiveWorkspaceChanged += PrimaryMainForm_ActiveWorkspaceChanged;
 
             // Knowledge Item Pop-Up Menu
 
@@ -75,16 +70,16 @@ namespace KnowledgePanelShowPDF
                 KnowledgeItem targetKnowledgeItem = foundKnowledgeItems[0];
                 mainForm.ActiveKnowledgeItem = targetKnowledgeItem;
 
-                // 3. 核心：通过遍历控件树来找到 QuotationSmartRepeater
-                SmartRepeater<KnowledgeItem> KnowledgeItemSmartRepeater = Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("SmartRepeater", true).FirstOrDefault() as SmartRepeater<KnowledgeItem>;
-                QuotationSmartRepeater quotationSmartRepeaterAsQuotationSmartRepeater =
-                Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("knowledgeItemPreviewSmartRepeater", true).FirstOrDefault() as QuotationSmartRepeater;
+                // 3. 核心：通过遍历控件树来找到 QuotationSmartRepeater，这个应该没用
+                //SmartRepeater<KnowledgeItem> KnowledgeItemSmartRepeater = Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("SmartRepeater", true).FirstOrDefault() as SmartRepeater<KnowledgeItem>;
+                //QuotationSmartRepeater quotationSmartRepeaterAsQuotationSmartRepeater =
+                //Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("knowledgeItemPreviewSmartRepeater", true).FirstOrDefault() as QuotationSmartRepeater;
 
-                if (quotationSmartRepeaterAsQuotationSmartRepeater != null)
-                {
-                    // 调用方法进行高亮
-                    quotationSmartRepeaterAsQuotationSmartRepeater.SelectAndActivate(targetKnowledgeItem, true);
-                }
+                //if (quotationSmartRepeaterAsQuotationSmartRepeater != null)
+                //{
+                //    // 调用方法进行高亮
+                //    quotationSmartRepeaterAsQuotationSmartRepeater.SelectAndActivate(targetKnowledgeItem, true);
+                //}
 
                 // --- 5. 执行PDF跳转 ---
                 // 检查目标知识条目是否确实关联了一个有效的地址（通常是PDF文件）。
@@ -104,71 +99,6 @@ namespace KnowledgePanelShowPDF
             {
                 MessageBox.Show("请选择一个Knowledge", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-        }
-        void PrimaryMainForm_ActiveWorkspaceChanged(object o, EventArgs a)
-        {
-            if (Program.ActiveProjectShell.PrimaryMainForm.ActiveWorkspace == MainFormWorkspace.KnowledgeOrganizer)
-            {
-                SmartRepeater<KnowledgeItem> KnowledgeItemSmartRepeater = (SmartRepeater<KnowledgeItem>)Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("SmartRepeater", true).FirstOrDefault();
-
-                QuotationSmartRepeater quotationSmartRepeaterAsQuotationSmartRepeater = Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("knowledgeItemPreviewSmartRepeater", true).FirstOrDefault() as QuotationSmartRepeater;
-
-                KnowledgeItemSmartRepeater.ActiveListItemChanged += KnowledgeItemPreviewSmartRepeater_ActiveListItemChanged;
-            }
-            else if (Program.ActiveProjectShell.PrimaryMainForm.ActiveWorkspace == MainFormWorkspace.ReferenceEditor)
-            {
-                QuotationSmartRepeater quotationSmartRepeaterAsQuotationSmartRepeater = Program.ActiveProjectShell.PrimaryMainForm.Controls.Find("quotationSmartRepeater", true).FirstOrDefault() as QuotationSmartRepeater;
-
-                quotationSmartRepeaterAsQuotationSmartRepeater.ActiveListItemChanged += QuotationSmartRepeater_ActiveListItemChanged;
-            }
-        }
-
-        void QuotationSmartRepeater_ActiveListItemChanged(object o, EventArgs a)
-        {
-            if (Program.ActiveProjectShell.PrimaryMainForm.GetSelectedQuotations().Count == 0) return;
-
-            KnowledgeItem activeQuotation = Program.ActiveProjectShell.PrimaryMainForm.GetSelectedQuotations().FirstOrDefault();
-            if (activeQuotation.EntityLinks == null) return;
-            if (activeQuotation.EntityLinks.Where(e => e.Indication == EntityLink.PdfKnowledgeItemIndication).Count() == 0) return;
-
-            Annotation annotation = activeQuotation.EntityLinks.Where(e => e.Indication == EntityLink.PdfKnowledgeItemIndication).FirstOrDefault().Target as Annotation;
-
-            PreviewControl previewControl = PreviewMethods.GetPreviewControl();
-            if (previewControl == null) return;
-
-            SwissAcademic.Citavi.Controls.Wpf.PdfViewControl pdfViewControl = previewControl.GetPdfViewControl();
-            if (pdfViewControl == null) return;
-
-            pdfViewControl.GoToAnnotation(annotation);
-
-        }
-
-        void KnowledgeItemPreviewSmartRepeater_ActiveListItemChanged(object o, EventArgs a)
-        {
-            if (Program.ActiveProjectShell.PrimaryMainForm.GetSelectedKnowledgeItems().Count == 0) return;
-
-            KnowledgeItem activeQuotation = Program.ActiveProjectShell.PrimaryMainForm.GetSelectedKnowledgeItems().FirstOrDefault();
-            if (activeQuotation.EntityLinks == null) return;
-            if (activeQuotation.EntityLinks.Where(e => e.Indication == EntityLink.PdfKnowledgeItemIndication).Count() == 0) return;
-
-            Annotation annotation = activeQuotation.EntityLinks.Where(e => e.Indication == EntityLink.PdfKnowledgeItemIndication).FirstOrDefault().Target as Annotation;
-
-            PreviewControl previewControl = PreviewMethods.GetPreviewControl();
-            if (previewControl == null) return;
-
-            PdfViewControl pdfViewControl = previewControl.GetPdfViewControl();
-            if (pdfViewControl == null) return;
-
-            Document document = pdfViewControl.Document;
-            if (document == null) return;
-
-            if (previewControl.ActiveLocation != annotation.Location)
-            {
-                Program.ActiveProjectShell.ShowPreviewFullScreenForm(annotation.Location, previewControl, null);
-            }
-            pdfViewControl.GoToAnnotation(annotation);
-
-            Program.ActiveProjectShell.PrimaryMainForm.Activate();
         }
     }
 }
